89c4d2b7c998beb57d5e943433282950c9826540
Adi Varshney <adi.varshney09@gmail.com>
2026-03-01 17:33:00 -0500

    Defined methods for Transfer Subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystem/Transfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystem/Transfer.java
@@ -6,15 +6,48 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 public class Transfer extends SubsystemBase {
-    DcMotorEx transferLeft, transferRight;
-    Servo flickerLeft, flickerRight;
-    CRServo middleRoller;
 
-    public Transfer(DcMotorEx TL, DcMotorEx TR, Servo FL, Servo FR, CRServo MR) {
-        transferLeft = TL;
-        transferRight = TR;
-        flickerLeft = FL;
-        flickerRight = FR;
-        middleRoller = MR;
+    private final DcMotorEx left, right;
+    private final Servo flickLeft, flickRight;
+    private final CRServo middle;
+
+    public Transfer(
+            DcMotorEx left,
+            DcMotorEx right,
+            Servo flickLeft,
+            Servo flickRight,
+            CRServo middle
+    ) {
+        this.left = left;
+        this.right = right;
+        this.flickLeft = flickLeft;
+        this.flickRight = flickRight;
+        this.middle = middle;
+    }
+
+    public void setLeftMotor(double power) {
+        left.setPower(power);
+    }
+
+    public void setRightMotor(double power) {
+        right.setPower(power);
+    }
+
+    public void setMiddlePower(double power) {
+        middle.setPower(power);
+    }
+
+    public void stopMotors() {
+        left.setPower(0);
+        right.setPower(0);
+        middle.setPower(0);
+    }
+
+    public void setLeftFlicker(double position) {
+        flickLeft.setPosition(position);
+    }
+
+    public void setRightFlicker(double position) {
+        flickRight.setPosition(position);
     }
 }
