b0e125863e59eb609a8a6bdf4abce876bd4bfbf5
Adi Varshney <adi.varshney09@gmail.com>
2026-03-01 17:33:31 -0500

    Updated IntakeRoller Subsystem methods
    to follow command-based paradidgm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystem/IntakeRollers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystem/IntakeRollers.java
@@ -1,38 +1,23 @@
 package org.firstinspires.ftc.teamcode.robot.subsystem;
 
-import static org.firstinspires.ftc.teamcode.robot.constants.IntakeRollerConstants.INTAKE;
-import static org.firstinspires.ftc.teamcode.robot.constants.IntakeRollerConstants.OFF;
-import static org.firstinspires.ftc.teamcode.robot.constants.IntakeRollerConstants.OUTTAKE;
-
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 public class IntakeRollers extends SubsystemBase {
-    CRServo leftServo, rightServo;
 
-    public IntakeRollers(CRServo LS, CRServo RS) {
-        leftServo = LS;
-        rightServo = RS;
+    private final CRServo left, right;
+
+    public IntakeRollers(CRServo left, CRServo right) {
+        this.left = left;
+        this.right = right;
     }
 
-    /**
-     * Turns on rollers to intake balls.
-     */
-    public void setForward() {
-        leftServo.setPower(OUTTAKE);
+    public void setPower(double power) {
+        left.setPower(power);
+        right.setPower(power);
     }
 
-    /**
-     * Turns on rollers to outtake balls.
-     */
-    public void setReverse() {
-        leftServo.setPower(INTAKE);
-    }
-
-    /**
-     * Turns off rollers.
-     */
-    public void setOff() {
-        leftServo.setPower(OFF);
+    public void stop() {
+        setPower(0);
     }
 }
