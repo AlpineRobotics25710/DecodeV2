265473da2fd54d43c1d92504da20bc9f17b8ff20
Adi Varshney <adi.varshney09@gmail.com>
2026-03-02 23:06:39 -0500

    Add BaseTeleOp with initialization and bindings
    Replace empty BaseTeleOp with a CommandOpMode-based base class that centralizes teleop setup. It wraps gamepad1/2 with GamepadEx, constructs an AlpineRobot from the hardwareMap, and sets the drivetrain's default command to PedroTeleOpDriveCommand using driver1. An abstract configureBindings() method is provided for child TeleOp classes to define button mappings.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/BaseTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/BaseTeleOp.java
@@ -1,4 +1,38 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
-public class BaseTeleOp {
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+
+import org.firstinspires.ftc.teamcode.robot.AlpineRobot;
+import org.firstinspires.ftc.teamcode.robot.commands.drive.PedroTeleOpDriveCommand;
+
+public abstract class BaseTeleOp extends CommandOpMode {
+
+    protected AlpineRobot robot;
+    protected GamepadEx driver1;
+    protected GamepadEx driver2;
+
+    @Override
+    public void initialize() {
+
+        // Wrap gamepads
+        driver1 = new GamepadEx(gamepad1);
+        driver2 = new GamepadEx(gamepad2);
+
+        // Create robot
+        robot = new AlpineRobot(hardwareMap);
+
+        // Set default drivetrain command
+        robot.drivetrain.setDefaultCommand(
+                new PedroTeleOpDriveCommand(robot.drivetrain, driver1)
+        );
+
+        // Let child TeleOp define button bindings
+        configureBindings();
+    }
+
+    /**
+     * Child classes define button mappings here.
+     */
+    protected abstract void configureBindings();
 }
